12098d2032dfb8bfca11c8f7cbb2d012e5cfc19e
Erica Faulkenberry <erica.faulkenberry@fishtownanalytics.com>
2025-10-07 21:13:15 +0000

    quote columns

--- models/marts/total_order_revenue.sql
+++ models/marts/total_order_revenue.sql
@@ -4,16 +4,8 @@
     materialized = 'table'
 )}}
 
-with orders as (
-    select * from {{ ref('xplat_foundation', 'fct_orders') }}
-), 
-
-agg as (
-    select 
-        ordered_at,
-        sum(order_total) as order_revenue 
-    from orders 
-    group by 1
-)
-
-select * from agg
+select 
+    "ordered_at",
+    sum("order_total") as order_revenue 
+from {{ ref('xplat_foundation', 'fct_orders') }}
+group by 1
--- models/marts/total_orders_by_region.sql
+++ models/marts/total_orders_by_region.sql
@@ -10,10 +10,10 @@ with orders as (
 
 agg as (
     select
-        ordered_at,
-        location_name, 
-        count(order_id) as order_count,
-        sum(order_total) as orders_revenue
+        "ordered_at",
+        "location_name", 
+        count("order_id") as order_count,
+        sum("order_total") as orders_revenue
     from orders 
     group by 1,2
 )
